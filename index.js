a9043dc7d2f7ab9e38b23c161510dcc0d78f87ce
GABRIEL ESTEBAN RUIZ MENESES <gabriel_ruiz21@hotmail.com>
2018-05-19 23:45:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,16 +13,6 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
-
-function sayHiToGrandma(string) {
-  if(string.toLowerCase() === string)
-    return "I can\'t hear you!"
-}
-
-function sayHiToGrandma(string) {
-  string.toUpperCase() === string
-  return "YES INDEED!"
-}
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.")
   return "I love you, too."
