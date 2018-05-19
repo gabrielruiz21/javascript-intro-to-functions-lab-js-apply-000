8545743061216e8e97ea160111bcb252528b6b05
GABRIEL ESTEBAN RUIZ MENESES <gabriel_ruiz21@hotmail.com>
2018-05-19 23:43:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string){
+function sayHiToGrandma(string) {
   if(string.toLowerCase() === string)
   return "I can't hear you!"
 }
 
-function sayHiToGrandma(string){
+function sayHiToGrandma(string) {
   string.toUpperCase() === string
   return "YES INDEED!"
 }
